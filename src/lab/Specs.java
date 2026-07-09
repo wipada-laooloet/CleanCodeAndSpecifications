6abd27008df688343fd43bfec60bf97d8da480cb
wipada-laooloet <wipada.la@ku.th>
2026-07-09 18:01:09 +0700

    Add files via upload

--- src/lab/Specs.java
+++ src/lab/Specs.java
@@ -18,10 +18,11 @@ public class Specs {
 
     // TODO ข้อ 4: เขียน JavaDoc ให้เมธอดนี้
     /**
-     * 
-     * @param arr 
-     * @param key
-     * @return
+     * เช็คค่าว่าตรงกับ  key ไหม
+     * @param arr ไม่เท่ากับ null
+     * @param key ค่าที่ต้องการจะหา
+     * @return หาตำแหน่ง key ถ้าไม่เจอจะแสดงผลมาเป็น -1
+     * @throws -1 ถ้า key arr!=null retrun ค่าที่ต้องการ
      */
     public static int firstIndexOf(int[] arr, int key) {
         if (arr == null) throw new IllegalArgumentException("arr must not be null");
@@ -32,9 +33,10 @@ public class Specs {
 
     // TODO ข้อ 5: เขียน JavaDoc ให้เมธอดนี้
     /**
-     * 
-     * @param radius
-     * @return
+     *สูตรหาค่าพื้นที่วงกลม
+     * @param radius ต้อง >= 0
+     * @return หาค่าพื้นที่วงกลม
+     * @throws error เมื่อ resuat  มีค่า >0
      */
     public static double circleArea(double radius) {
         if (radius < 0) throw new IllegalArgumentException("radius must be >= 0");
@@ -44,6 +46,12 @@ public class Specs {
     // TODO ข้อ 6: เขียน JavaDoc ให้เมธอดนี้
     //   (เมธอดนี้ตัดช่องว่างหน้า-หลัง แล้วแปลงเป็นตัวพิมพ์เล็ก;
     //    input null ให้คืน string ว่าง)
+   /**
+    * ตัดช่องว่างหน้าหลัง แปลงตัวพิมพ์เล็ก
+    * @param s ช้อความเท่ากับ null
+    * @return ถ้า == null จะเป็นข้อความว่าตัวอักษรพิมพ์เล็ก ไม่มีหน้าหลัง
+    *
+    */
     public static String normalize(String s) {
         if (s == null) return "";
         return s.trim().toLowerCase();
